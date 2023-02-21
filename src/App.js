2b048758512a92be4e91d95e35008844d7f6df11
Gustavo Luna <36255614+irontrooper7@users.noreply.github.com>
2023-02-21 17:07:47 -0600

    [add] animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import { useEffect } from 'react';
+import { gsap, Linear, Sine } from 'gsap';
 
 function App() {
+
+  useEffect(() => {
+    let total = 50;
+    let container = document.getElementById('leave-container');
+    let w = container.scrollWidth;
+    let h = container.scrollHeight;
+    for(let i = 1; i<total;i++) {
+      let leave = document.createElement('div');
+      leave.className = 'leave';
+      gsap.set(leave, { x:randomRangeNumber(0,w),y:randomRangeNumber(-200,-150),z:randomRangeNumber(-200,200) });
+      container.appendChild(leave);
+      fallAnimation(leave, h);
+    }
+  })
+
+  function fallAnimation(item, height) {
+    let tl = gsap.timeline();
+    tl.to(item, { y: height+100, ease: Linear.easeNone, repeat: -1,delay:-15, duration: randomRangeNumber(6,15) });
+    tl.to(item, { x: '+=100', rotationZ: randomRangeNumber(0,180), repeat:-1, yoyo: true, ease: Sine.easeInOut, duration: randomRangeNumber(4,8)});
+    tl.to(item, { rotationX: randomRangeNumber(0,360), rotationY: randomRangeNumber(0,360), repeat: -1, yoyo: true, ease: Sine.easeInOut, delay:-5, duration: randomRangeNumber(2,8) });
+  }
+
+  function randomRangeNumber(min, max) {
+    return min+Math.random()*(max-min)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="hero is-fullheight">
+        <div className="hero-body">
+          <div id="leave-container"></div>
+        </div>
+      </section>      
     </div>
   );
 }
